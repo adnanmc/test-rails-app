6ce9c9ce32ad18181bc68d6f88785efd0751234c
adnanmc <adnanmcj@gmail.com>
2016-05-19 20:02:57 -0400

    added index and show on micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,21 @@
 class MicropostsController < ApplicationController
   def index
+    @micropost = Micropost.all
   end
 
   def new
+    @micropost = Micropost.new
+  end
+
+  def create
+    #code
   end
 
   def show
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def edit
+    #code
   end
 end
